3e9c5ece07cd4b6a0c38da67c91527d28a6a9b64
madan tamilselvan <madnt51@yahoo.com.sg>
2024-06-05 23:24:19 +0800

    Front End Register Page

--- src/pages/Login/indexLogin.jsx
+++ src/pages/Login/indexLogin.jsx
@@ -1,58 +1,66 @@
 import React from 'react';
-import { Button,Form } from "antd";
+import { Button, Form } from "antd";
 import { Link } from 'react-router-dom';
 
 function indexLogin() {
-  const onFinish = (values)=>{
-    console.log("Great",values);
+  const onFinish = (values) => {
+    console.log("Great", values);
   }
 
-  
-  
-  
-  
+
   return (
-    <div className='grid grid-cols-2 h-screen'>
-      <div className='bg-primary'></div>
+    <div className="grid grid-cols-2 h-screen">
+      <div className="bg-primary flex flex-col items-center justify-center">
+       <div>
+
+        <h1 className="text-5xl text-yellow-600 font-semibold">
+          Welcome to Mad Gaming Review
+        </h1>
+        <span className='text-md text-gray-300 mt-2'>
+          Daily Gaming Review,Ratings and recommendation
+        </span>
+       </div>
+
+
+
+      </div>
+         
       <div className='flex items-center justify-center'>
-        <div className='w-[400px]' >
+        <div className='w-[500px]' >
           <h1 className="text-2xl mb-2">Login To Your Account</h1>
-          <hr/>
-          
+          <hr />
+
 
           <Form
             layout="vertical"
             className='flex flex-col gap-5 mt-3'
             onFinish={onFinish}>
+
             <Form.Item
               label="Email"
               name="email">
               <input />
             </Form.Item>
+
             <Form.Item label="Password" name="password">
-              <input />
+              <input type ="password" />
+              
             </Form.Item>
-          
-          <div className="flex flex-col gap-5">
-          <Button type ="primary" htmlType = "submit" block>
-              Login
-            </Button>
 
-            <Link to="/register" >
-              Sign Up For New Account
+            <div className="flex flex-col gap-5">
+              <Button type="primary" htmlType="submit" block>
+                Login
+              </Button>
 
-            </Link>
+              <Link to="/register" >
+                Sign Up For New Account
 
-
-          </div>
+              </Link>
+              </div>
 
           </Form>
         </div>
-
-
-
       </div>
-
     </div>
   )
 }
--- src/pages/Register/indexRegister.jsx
+++ src/pages/Register/indexRegister.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React from 'react';
+import { Button, Form } from "antd";
+import { Link } from 'react-router-dom';
 
 function indexRegister() {
+  const onFinish = (values) => {
+    console.log("Great", values);
+  }
+
+
   return (
-    <div>indexRegister</div>
+    <div className="grid grid-cols-2 h-screen">
+      <div className="bg-primary flex flex-col items-center justify-center">
+       <div>
+
+        <h1 className="text-5xl text-yellow-600 font-semibold">
+          Welcome to Mad Gaming Review
+        </h1>
+        <span className='text-md text-gray-300 mt-2'>
+          Daily Gaming Review,Ratings and recommendation
+        </span>
+       </div>
+
+      </div>
+         
+      <div className='flex items-center justify-center'>
+        <div className='w-[500px]' >
+          <h1 className="text-2xl mb-2">Register Your Account</h1>
+          <hr />
+
+
+          <Form
+            layout="vertical"
+            className='flex flex-col gap-5 mt-3'
+            onFinish={onFinish}>
+            
+            <Form.Item
+              label="Name"
+              name="name">
+              <input />
+            </Form.Item>
+
+
+            <Form.Item
+              label="Email"
+              name="email">
+              <input />
+            </Form.Item>
+
+            <Form.Item label="Password" name="password">
+              <input type="password" />
+            </Form.Item>
+
+            <div className="flex flex-col gap-5">
+              <Button type="primary" htmlType="submit" block>
+                Register
+              </Button>
+
+              <Link to="/login" >
+                Already Have a Account? Login Here!
+
+              </Link>
+              </div>
+
+          </Form>
+        </div>
+      </div>
+    </div>
   )
 }
 
