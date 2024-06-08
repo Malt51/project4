d4422866817f74e4c411c6e10da2f2006a14e9e3
madan tamilselvan <madnt51@yahoo.com.sg>
2024-06-08 12:57:36 +0800

    backend up setup

---
+++ Server/config/dbConfig.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.mongo_url,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+connection.on("connected",()=>{
+console.log( "Mongo DB Connected Successfully")
+});
+
+
+connection.on("error",(err)=>{
+    console.log( "Mongo DB Connection Fail",err);
+    });
